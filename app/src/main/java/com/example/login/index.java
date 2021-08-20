6eb397a4f56100275d6b01652dc55975f9451cf3
1053087475 <1053087475@qq.com>
2021-08-20 23:14:16 +0800

    add conrribution view

--- app/src/main/java/com/example/login/index.java
+++ app/src/main/java/com/example/login/index.java
@@ -1,72 +1,84 @@
 package com.example.login;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hancher.contribution.ContributionConfig;
+import com.hancher.contribution.ContributionItem;
+import com.hancher.contribution.ContributionView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-public class index extends AppCompatActivity {
-    String code;
-    private String info;
-    TextView show_login,show_name,show_repos_url;
-    Map<String, String> userinfo;
 
-    @SuppressLint("HandlerLeak")
-    Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            System.out.println("正在message!!!");
-            super.handleMessage(msg);
-            update(userinfo);
-        }
-    };
-    private void saveInfo(Map<String, String> info){
+import javax.net.ssl.HttpsURLConnection;
+
+public class index extends AppCompatActivity {
+
+    TextView show_login,show_name,show_repos_url;
+    Map<String, String> userinfo,contribution;
+    ContributionView contributionView;
+    private void saveContribution(Map<String, String> map){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                userinfo=info;
+                contribution=map;
             }
         });
     }
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.index);
+
         show_login = findViewById(R.id.show_login);
         show_name = findViewById(R.id.show_name);
         show_repos_url = findViewById(R.id.show_repos_url);
-
-
-
+        contributionView = findViewById(R.id.contributionView);
         Intent intent = getIntent();
-        String action = intent.getAction();
-        if(Intent.ACTION_VIEW.equals(action)){
-            Uri code_uri = intent.getData();
-            if(code_uri != null){
-                code = code_uri.getQueryParameter("code");
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        getToken(code);
-                        Message message = new Message();
-                        handler.sendEmptyMessage(1);
-                    }
-                }).start();
-            }
-        }
+        String githubInfoResult = intent.getStringExtra("githubInfoResult");
+        userinfo = HttpUtil.jsonToMap(githubInfoResult);
+        update(userinfo);
+        //
+        initview();
+
+
     }
 
+    private void initview(){
+        //贡献图点击事件
+        contributionView.setOnItemClick(new ContributionView.OnItemClickListener() {
+            @Override
+            public void onClick(int position, ContributionItem item) {
+                StringBuffer stringBuffer = new StringBuffer();
+                stringBuffer.append(item.getNumber()).append(" contributions on ").append(transformatDate(item.getTime()));
+                Toast.makeText(index.this, stringBuffer, Toast.LENGTH_SHORT).show();
+            }
+        });
+        //获取贡献信息
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getContributions();
+            }
+        }).start();
 
+    }
     public void update(Map<String, String> userinfo){
         runOnUiThread(new Runnable() {
             @Override
@@ -78,31 +90,116 @@ public class index extends AppCompatActivity {
         });
     }
 
-    public void getToken(String code){
-        String token_url = GitHubConstants.TOKEN_URL.replace("CLIENT_ID", GitHubConstants.CLIENT_ID)
-                .replace("CLIENT_SECRET", GitHubConstants.CLIENT_SECRET)
-                .replace("CALLBACK", GitHubConstants.CALLBACK)
-                .replace("CODE", code);
-        System.out.println("用户信息数据"+token_url);//这个里面有我们想要的用户信息数据
-        String githubAccessTokenResult = HttpUtil.getAccess_token(token_url);
-        System.out.println("==>githubAccessTokenResult: " + githubAccessTokenResult);
+    public void updateC(Date startDate,List<ContributionItem> data){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                contributionView.setData(startDate, data);
+                contributionView.setConfig(configContribution());
+            }
+        });
+    }
 
-        String[] githubResultList = githubAccessTokenResult.split("&");
-        List<String> params = new ArrayList<>();
-        for (String paramMap : githubResultList) { if (!"scope".equals(paramMap.split("=")[0])){
-            // 再以 = 为分割字符分割, 并加到 params 中
-            params.add(paramMap.split("=")[1]);
+    public void getContributions(){
+        try {
+            URL url = new URL("https://github.com/users/"+ userinfo.get("login")+"/contributions");
+            HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
+            InputStream inputStream = httpsURLConnection.getInputStream();
+            InputStreamReader reader = new InputStreamReader(inputStream,"UTF-8");
+            BufferedReader bufferedReader = new BufferedReader(reader);
+            String temp = null,datacount,datadata;
+            Map<String,String> map = new HashMap<String,String>();
+            while ((temp = bufferedReader.readLine()) != null){
+                if(temp.contains("data-count")){
+                    datacount = getDatacount(temp);
+                    if (!datacount.equals("0")){
+                        datadata = getDatadata(temp);
+                        map.put(datadata,datacount);
+                    }
+                }
+            }
+            bufferedReader.close();
+            reader.close();
+            inputStream.close();
+            saveContribution(map);
+            setContributionView();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        }
-        //此时 params.get(0) 为 access_token;  params.get(1) 为 token_type
-        // Step2：通过 access_token 获取用户信息
-        String access_token = params.get(0);
-        String githubInfoResult = HttpUtil.getUser_info(GitHubConstants.USER_INFO_URL,access_token);
+    }
+    private void setContributionView(){
+        int day=0,digit=146;
+        List<ContributionItem> data = new ArrayList<>();
 
-        Map<String, String> userinfo = HttpUtil.jsonToMap(githubInfoResult);
-        System.out.println("登录用户信息:"+userinfo);//responseMap里面保存着用户登录信息
-        System.out.println("获取登录用户的用户名:"+userinfo.get("login"));
-        //给全局变量赋值
-        saveInfo(userinfo);
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH));
+        calendar.add(Calendar.DAY_OF_MONTH,-digit);
+        while(calendar.get(Calendar.DAY_OF_WEEK)!=1){
+            calendar.add(Calendar.DAY_OF_MONTH,-1);
+            day++;
+        }
+
+        Date startDate = calendar.getTime();
+        for (int i = 0; i <= digit + day; i++) {
+            String time = getTime(calendar);
+            if(contribution.get(time) != null){
+                data.add(new ContributionItem(calendar.getTime(),Integer.valueOf(contribution.get(time))));
+
+            }else{
+                data.add(new ContributionItem(calendar.getTime(),0));
+            }
+
+            calendar.add(Calendar.DAY_OF_MONTH, 1);
+        }
+        updateC(startDate,data);
+    }
+    private String getTime(Calendar calendar){
+        String MM,dd,time;
+        if(calendar.get(Calendar.MONTH)+1 < 10){
+
+            MM = "0" + (calendar.get(Calendar.MONTH) + 1);
+        }else{
+            MM = (calendar.get(Calendar.MONTH) + 1) + "";
+        }
+        if(calendar.get(Calendar.DAY_OF_MONTH) < 10){
+            dd = "0" + calendar.get(Calendar.DAY_OF_MONTH);
+        }else{
+            dd = calendar.get(Calendar.DAY_OF_MONTH) + "";
+        }
+        time = calendar.get(Calendar.YEAR) + "-" + MM + "-" + dd;
+        return time;
+    }
+    public String transformatDate(Date date) {
+        SimpleDateFormat simpleDateFormat=new SimpleDateFormat("yyyy-MM-dd");
+        String transformDate=simpleDateFormat.format(date);
+        return transformDate;
+    }
+
+    private String getDatacount(String line){
+        String[] str = line.split("data-count=\"");
+        String result = str[1].split("\"")[0];
+        return  result;
+    }
+
+    private String getDatadata(String line){
+        String[] str = line.split("data-date=\"");
+        String result = str[1].split("\"")[0];
+        return  result;
+    }
+    private ContributionConfig configContribution(){
+        ContributionConfig config = new ContributionConfig()
+                .setBorderWidth(2)//边框宽度
+                .setBorderColor(0xFF9E9E9E)//边框颜色
+                .setItemRound(5)//圆角矩形圆半径
+                .setMonths(new String[]{"1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"})//月份字符串
+                .setPadding(4)//单个框宽度
+                .setRank(new int[]{0,2,5,8,10})//颜色等级范围，大于等于2小于5则为第二个颜色范围
+                .setRankColor(new int[]{0xFFEBEDF0, 0xFF9BE9A8, 0xFF40C463, 0xFF30A14E, 0xFF216E39})//填充的等级颜色
+                .setWeeks(new String[]{"", "周一", "", "周三", "", "周五", ""})//周名称
+                .setStartOfWeek(Calendar.SUNDAY)//配合setWeeks一起使用，可以实现第一行为周日，默认第一行周一
+                .setTxtColor(0xFF9E9E9E);//设置文字颜色
+        return config;
     }
 }
--- app/src/main/java/com/example/login/main.java
+++ app/src/main/java/com/example/login/main.java
@@ -1,26 +1,74 @@
 package com.example.login;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 public class main extends AppCompatActivity {
+    String code;
+    String githubInfoResult;
+    private String info;
+    Button login;
+
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            System.out.println("正在message!!!");
+            super.handleMessage(msg);
+            Intent intent = new Intent(main.this,index.class);
+            intent.putExtra("githubInfoResult",githubInfoResult);
+            startActivity(intent);
+        }
+    };
+    private void saveInfo(String info){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                githubInfoResult=info;
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        login = findViewById(R.id.login);
         initview();
+
+        Intent intent = getIntent();
+        String action = intent.getAction();
+        if(Intent.ACTION_VIEW.equals(action)){
+            login.setText("登陆中……");
+            Uri code_uri = intent.getData();
+            if(code_uri != null){
+                code = code_uri.getQueryParameter("code");
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        getToken(code);
+                        Message message = new Message();
+                        handler.sendEmptyMessage(1);
+                    }
+                }).start();
+            }
+        }
     }
 
     public void initview(){
-        Button login = findViewById(R.id.login);
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,6 +85,36 @@ public class main extends AppCompatActivity {
         Uri uri = Uri.parse(code_url);
         Intent i = new Intent(Intent.ACTION_VIEW, uri);
         startActivity(i);
+
+    }
+
+
+    public void getToken(String code){
+        String token_url = GitHubConstants.TOKEN_URL.replace("CLIENT_ID", GitHubConstants.CLIENT_ID)
+                .replace("CLIENT_SECRET", GitHubConstants.CLIENT_SECRET)
+                .replace("CALLBACK", GitHubConstants.CALLBACK)
+                .replace("CODE", code);
+        System.out.println("用户信息数据"+token_url);//这个里面有我们想要的用户信息数据
+        String githubAccessTokenResult = HttpUtil.getAccess_token(token_url);
+        System.out.println("==>githubAccessTokenResult: " + githubAccessTokenResult);
+
+        String[] githubResultList = githubAccessTokenResult.split("&");
+        List<String> params = new ArrayList<>();
+        for (String paramMap : githubResultList) { if (!"scope".equals(paramMap.split("=")[0])){
+            // 再以 = 为分割字符分割, 并加到 params 中
+            params.add(paramMap.split("=")[1]);
+        }
+        }
+        //此时 params.get(0) 为 access_token;  params.get(1) 为 token_type
+        // Step2：通过 access_token 获取用户信息
+        String access_token = params.get(0);
+        String githubInfoResult = HttpUtil.getUser_info(GitHubConstants.USER_INFO_URL,access_token);
+
+        //给全局变量赋值
+        saveInfo(githubInfoResult);
+        Map<String, String> userinfo = HttpUtil.jsonToMap(githubInfoResult);
+        System.out.println("登录用户信息:"+userinfo);//responseMap里面保存着用户登录信息
+        System.out.println("获取登录用户的用户名:"+userinfo.get("login"));
 
     }
 }
